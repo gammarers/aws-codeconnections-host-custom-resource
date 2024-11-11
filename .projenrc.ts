d915192a36af4e9b782044573e9ccdc3799b839b
yicr <yicr@users.noreply.github.com>
2024-11-11 18:29:50 +0900

    chore(deps): update may packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,8 +9,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: '@gammarers/aws-codeconnections-host-custom-resource',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-codeconnections-host-custom-resource.git',
-  deps: [
-    '@gammarers/aws-resource-naming@^0.6.0',
+  //  deps: [
+  //    '@gammarers/aws-resource-naming@^0.8.0',
+  //  ],
+  //  devDeps: [
+  //    '@gammarers/aws-resource-naming@0.8.0',
+  //  ],
+  peerDeps: [
+    '@gammarers/aws-resource-naming@^0.8.0',
   ],
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
